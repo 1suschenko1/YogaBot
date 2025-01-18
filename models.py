f412b43cd3e644a989db8cbe39968c2f93fd1e30
1suschenko1 <104361845+1suschenko1@users.noreply.github.com>
2025-01-18 04:16:58 +0300

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-class WorkoutPlan:
-    def __init__(self, goal, level):
-        self.goal = goal
-        self.level = level
-
-    def generate_plan(self):
-        return f"План для цели: {self.goal}, уровень: {self.level}"
